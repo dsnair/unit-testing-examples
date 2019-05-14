85da358687d287cb23bcd50918675c86c8e9bf0f
Divya Nair <divnairs@gmail.com>
2019-05-13 21:55:03 -0700

    test success()

--- JS-testing/enhancer.js
+++ JS-testing/enhancer.js
@@ -7,12 +7,21 @@ class Item {
   repair() {
     return { ...item, durability: 100 }
   }
+  success() {
+    const newItem =
+      item.enhancement >= 0 && item.enhancement < 20
+        ? { ...item, enhancement: ++item.enhancement }
+        : { ...item }
+    return newItem
+  }
 }
 
 const item = new Item({
   name: 'Iron Sword',
-  durability: Math.floor(Math.random() * 101),
-  enhancement: Math.floor(Math.random() * 21)
+  durability: 5,
+  enhancement: 18
+  // durability: Math.floor(Math.random() * 101),
+  // enhancement: Math.floor(Math.random() * 21)
 })
 
 module.exports = item
--- JS-testing/enhancer.test.js
+++ JS-testing/enhancer.test.js
@@ -1,33 +1,61 @@
 const enhancer = require('./enhancer.js')
 
-test('Item has name, durability, enhancement', () => {
-  // original item
-  expect(enhancer).toHaveProperty('name')
-  expect(enhancer).toHaveProperty('durability')
-  expect(enhancer).toHaveProperty('enhancement')
-  // after repair
-  expect(enhancer.repair()).toHaveProperty('name')
-  expect(enhancer.repair()).toHaveProperty('durability')
-  expect(enhancer.repair()).toHaveProperty('enhancement')
+// DATA INTEGRITY
+describe('Original data integrity', () => {
+  test('Item has name, durability, enhancement', () => {
+    expect(enhancer).toHaveProperty('name')
+    expect(enhancer).toHaveProperty('durability')
+    expect(enhancer).toHaveProperty('enhancement')
+  })
+  test('Item durability is a number between [0, 100]', () => {
+    expect(enhancer.durability).toBeGreaterThanOrEqual(0)
+    expect(enhancer.durability).toBeLessThanOrEqual(100)
+  })
+  test('Item enhancement is a number between [0, 20]', () => {
+    expect(enhancer.enhancement).toBeGreaterThanOrEqual(0)
+    expect(enhancer.enhancement).toBeLessThanOrEqual(20)
+  })
+  test('Item name is a non-empty string', () => {
+    expect(typeof enhancer.name).toBe('string')
+    expect(enhancer.name).toBeTruthy() // no '', null, undefined, NaN, false, 0
+  })
   // console.log(enhancer)
 })
 
-test('Item durability is a number between [0, 100]', () => {
-  // original item
-  expect(enhancer.durability).toBeGreaterThanOrEqual(0)
-  expect(enhancer.durability).toBeLessThanOrEqual(100)
+// REPAIR
+describe('On Repair', () => {
+  test('Item durability is restored to 100', () => {
+    expect(enhancer.repair().durability).toBe(100)
+  })
+  test('Item name and enhancement are not changed', () => {
+    expect(enhancer.repair().name).toBe(enhancer.name)
+    expect(enhancer.repair().enhancement).toBe(enhancer.enhancement)
+    // console.log(enhancer.repair())
+  })
 })
 
-test('When item is repaired, its durability is restored to 100', () => {
-  expect(enhancer.repair().durability).toBe(100)
-  // console.log(enhancer.repair())
-})
+// SUCCESS
+describe('On Success', () => {
+  test('Item enhancement increases by 1', () => {
+    const one = enhancer.success()
+    const two = enhancer.success()
+    // console.log(one, two)
+    one.enhancement === 20
+      ? expect(one.enhancement).toBe(two.enhancement)
+      : expect(one.enhancement).toBe(two.enhancement - 1)
+  })
 
-test('Item name is a non-empty string', () => {
-  // original
-  expect(typeof enhancer.name).toBe('string')
-  expect(enhancer.name).toBeTruthy() // no '', null, undefined, NaN, false, 0
-  // after repair
-  expect(typeof enhancer.repair().name).toBe('string')
-  expect(enhancer.repair().name).toBeTruthy()
+  test('Item name and durability are not changed', () => {
+    const one = enhancer.success()
+    const two = enhancer.success()
+    expect(two.name).toBe(enhancer.name)
+    expect(two.durability).toBe(enhancer.durability)
+  })
+
+  test('Item enhancement is a number between [0, 20]', () => {
+    const one = enhancer.success()
+    const two = enhancer.success()
+    expect(two.enhancement).toBeGreaterThanOrEqual(0)
+    expect(two.enhancement).toBeLessThanOrEqual(20)
+  })
 })
