20d6b963c929b482a7a9f82cd5c8a3dbfc33101e
Divya Nair <divnairs@gmail.com>
2019-05-15 15:28:47 -0700

    test if score resets to 0 at 3 strikes or 4 balls

--- react-testing-I/src/App.test.js
+++ react-testing-I/src/App.test.js
@@ -8,14 +8,6 @@ test('renders without crashing', () => {
   render(<App />)
 })
 
-test('displays initial scoreboard: Strike 0, Ball 0', () => {
-  // attach getByTestId function to the rendered div (DOM node)
-  const { getByTestId } = render(<App />)
-  // use getByTestId() to find DOM nodes with ids: 'strike', 'ball'
-  expect(getByTestId('strike').innerHTML).toBe('0')
-  expect(getByTestId('ball').innerHTML).toBe('0')
-})
-
 describe('Strike Button', () => {
   test('increments Strike score', () => {
     const { getByTestId, getAllByText } = render(<App />)
@@ -52,6 +44,38 @@ describe('Ball Button', () => {
     fireEvent.click(ballBtn)
     fireEvent.click(ballBtn)
     fireEvent.click(ballBtn)
+    expect(getByTestId('ball').innerHTML).toBe('0')
+  })
+})
+
+describe('Scoreboard Display', () => {
+  test('initial scoreboard is 0', () => {
+    // attach getByTestId function to the rendered div (DOM node)
+    const { getByTestId } = render(<App />)
+    // use getByTestId() to find DOM nodes with ids: 'strike', 'ball'
+    expect(getByTestId('strike').innerHTML).toBe('0')
+    expect(getByTestId('ball').innerHTML).toBe('0')
+  })
+
+  test('reset scoreboard to 0 at 3 strikes or 4 balls', () => {
+    // attach getByTestId function to the rendered div (DOM node)
+    const { getByTestId, getAllByText } = render(<App />)
+    // find strike and ball button DOM nodes
+    const strikeBtn = getAllByText('Strike')[1]
+    const ballBtn = getAllByText('Ball')[1]
+    // fire Strike button click event 3 times
+    fireEvent.click(strikeBtn)
+    fireEvent.click(strikeBtn)
+    fireEvent.click(strikeBtn)
+    // use getByTestId() to find DOM nodes with ids: 'strike', 'ball'
+    expect(getByTestId('strike').innerHTML).toBe('0')
+    expect(getByTestId('ball').innerHTML).toBe('0')
+    // fire Ball button click event 4 times
+    fireEvent.click(ballBtn)
+    fireEvent.click(ballBtn)
+    fireEvent.click(ballBtn)
+    fireEvent.click(ballBtn)
+    expect(getByTestId('strike').innerHTML).toBe('0')
     expect(getByTestId('ball').innerHTML).toBe('0')
   })
 })
