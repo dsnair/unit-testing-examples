f49318c3b5cb85451ed56233f22ed4d995341dc2
Divya Nair <divnairs@gmail.com>
2019-05-14 22:56:31 -0700

    add basic app style

--- react-testing-I/src/App.js
+++ react-testing-I/src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
-function App() {
-  return <>{'hi'}</>
+const App = () => {
+  const [ball, setBall] = useState(0)
+  const [strike, setStrike] = useState(0)
+  return (
+    <div className="app">
+      <div className="display">
+        <p className="score">{ball}</p>
+        <p className="score">{strike}</p>
+        <p>Ball</p>
+        <p>Strike</p>
+      </div>
+      <button>Strike</button>
+      <button>Ball</button>
+      <button>Foul</button>
+      <button>Hit</button>
+    </div>
+  )
 }
 
 export default App
