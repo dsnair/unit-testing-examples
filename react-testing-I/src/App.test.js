710255685f8ee49bb7bd67933d2a835b03b4bcd5
Divya Nair <divnairs@gmail.com>
2019-05-15 15:36:17 -0700

    test that Hit button resets scoreboard to 0

--- react-testing-I/src/App.test.js
+++ react-testing-I/src/App.test.js
@@ -10,17 +10,18 @@ test('renders without crashing', () => {
 
 describe('Strike Button', () => {
   test('increments Strike score', () => {
+    // attach getByTestId, getAllByText function to the rendered div (DOM node)
     const { getByTestId, getAllByText } = render(<App />)
-    // find button, not paragraph, named Strike
+    // find button, not paragraph, from nodelist that has text 'Strike'
     const strikeBtn = getAllByText('Strike')[1]
     // fire Strike button click event
     fireEvent.click(strikeBtn)
+    // check if text content of DOM node with id 'strike' is '1'
     expect(getByTestId('strike').innerHTML).toBe('1')
   })
 
   test(`Strike score doesn't exceed 2`, () => {
     const { getByTestId, getAllByText } = render(<App />)
-    // find button, not paragraph, named Strike
     const strikeBtn = getAllByText('Strike')[1]
     // fire Strike button click event 3 times
     fireEvent.click(strikeBtn)
@@ -50,24 +51,19 @@ describe('Ball Button', () => {
 
 describe('Scoreboard Display', () => {
   test('initial scoreboard is 0', () => {
-    // attach getByTestId function to the rendered div (DOM node)
     const { getByTestId } = render(<App />)
-    // use getByTestId() to find DOM nodes with ids: 'strike', 'ball'
     expect(getByTestId('strike').innerHTML).toBe('0')
     expect(getByTestId('ball').innerHTML).toBe('0')
   })
 
   test('reset scoreboard to 0 at 3 strikes or 4 balls', () => {
-    // attach getByTestId function to the rendered div (DOM node)
     const { getByTestId, getAllByText } = render(<App />)
-    // find strike and ball button DOM nodes
     const strikeBtn = getAllByText('Strike')[1]
     const ballBtn = getAllByText('Ball')[1]
     // fire Strike button click event 3 times
     fireEvent.click(strikeBtn)
     fireEvent.click(strikeBtn)
     fireEvent.click(strikeBtn)
-    // use getByTestId() to find DOM nodes with ids: 'strike', 'ball'
     expect(getByTestId('strike').innerHTML).toBe('0')
     expect(getByTestId('ball').innerHTML).toBe('0')
     // fire Ball button click event 4 times
@@ -75,6 +71,15 @@ describe('Scoreboard Display', () => {
     fireEvent.click(ballBtn)
     fireEvent.click(ballBtn)
     fireEvent.click(ballBtn)
+    expect(getByTestId('strike').innerHTML).toBe('0')
+    expect(getByTestId('ball').innerHTML).toBe('0')
+  })
+})
+
+describe('Hit Button', () => {
+  test('resets scoreboard to 0', () => {
+    const { getByTestId, getByText } = render(<App />)
+    fireEvent.click(getByText('Hit')) // fire Hit button click event once
     expect(getByTestId('strike').innerHTML).toBe('0')
     expect(getByTestId('ball').innerHTML).toBe('0')
   })
