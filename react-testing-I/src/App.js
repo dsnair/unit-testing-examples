1334abd7ff764cbecd13a9ad5b697209af767463
Divya Nair <divnairs@gmail.com>
2019-05-14 23:34:00 -0700

    add count rule for Foul onclick

--- react-testing-I/src/App.js
+++ react-testing-I/src/App.js
@@ -6,22 +6,26 @@ const App = () => {
   const [ball, setBall] = useState(0)
 
   const handleStrike = () => {
-    if (strike === 3) {
+    if (strike === 2) {
       setStrike(0)
       setBall(0)
-    } else setStrike((strike + 1) % 4)
+    } else setStrike((strike + 1) % 3)
   }
 
   const handleBall = () => {
-    if (ball === 4) {
+    if (ball === 3) {
       setStrike(0)
       setBall(0)
-    } else setBall((ball + 1) % 5)
+    } else setBall((ball + 1) % 4)
   }
 
   const handleHit = () => {
     setStrike(0)
     setBall(0)
+  }
+
+  const handleFoul = () => {
+    if (strike < 2) setStrike(strike + 1)
   }
 
   return (
@@ -34,8 +38,8 @@ const App = () => {
       </div>
       <button onClick={handleStrike}>Strike</button>
       <button onClick={handleBall}>Ball</button>
-      <button>Foul</button>
       <button onClick={handleHit}>Hit</button>
+      <button onClick={handleFoul}>Foul</button>
     </div>
   )
 }
