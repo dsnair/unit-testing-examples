685894d3215846c0878173331c6b9665bb7a7259
Divya Nair <divnairs@gmail.com>
2019-05-14 23:15:10 -0700

    add rules for Ball onclick

--- react-testing-I/src/App.js
+++ react-testing-I/src/App.js
@@ -2,18 +2,26 @@ import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
-  const [ball, setBall] = useState(0)
   const [strike, setStrike] = useState(0)
+  const [ball, setBall] = useState(0)
+
+  const handleBall = () => {
+    if (ball === 4) {
+      setStrike(0)
+      setBall(0)
+    } else setBall((ball + 1) % 5)
+  }
+
   return (
     <div className="app">
       <div className="display">
-        <p className="score">{ball}</p>
         <p className="score">{strike}</p>
-        <p>Ball</p>
+        <p className="score">{ball}</p>
         <p>Strike</p>
+        <p>Ball</p>
       </div>
       <button>Strike</button>
-      <button>Ball</button>
+      <button onClick={handleBall}>Ball</button>
       <button>Foul</button>
       <button>Hit</button>
     </div>
